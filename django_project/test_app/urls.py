99b0c3ea9cd484f4b025d899cc0482bfc9922bae
richard <richard@linux-mint>
2018-11-27 22:38:13 -0500

    Add a simple change

--- django_project/test_app/urls.py
+++ django_project/test_app/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views   # ("." = import from current directory)
 
 urlpatterns = [
-    path('', views.home, name='test_app-home'),
+    path('home/', views.home, name='test_app-home'),
+    path('', views.base, name='test_app-base')
 ]
--- django_project/test_app/views.py
+++ django_project/test_app/views.py
@@ -4,6 +4,9 @@ from easygui import *
 def home(request):
     return render(request, 'test_app/home.html')
 
+def base(request):
+    return render(request, 'test_app/base.html')
+
     
 
     
